bd862b550257c6c50855dfb98b010db99b2bc5bc
Jason Fox <jasonrobertfox@gmail.com>
2014-01-12 14:18:37 -0300

    Add extension check.

--- projects/07/Nand2TetrisVMTranslator/vmtranslator/src/main/java/com/jasonrobertfox/nand2tetris/vmtranslator/CLI.java
+++ projects/07/Nand2TetrisVMTranslator/vmtranslator/src/main/java/com/jasonrobertfox/nand2tetris/vmtranslator/CLI.java
@@ -1,25 +1,35 @@
+
 package com.jasonrobertfox.nand2tetris.vmtranslator;
 
 import java.io.File;
 
+import org.apache.commons.io.FilenameUtils;
+
 public class CLI
 {
 
+  private static final String SOURCE_EXTENSION = "vm";
+
   public static void main(String... args)
   {
-    if (args.length == 0){
+    if (args.length == 0) {
       System.out.println("Must provide a *.vm file or directory.");
       System.exit(1);
     }
-    
+
     String path = args[0];
     File source = new File(path);
-    if(!source.exists()) { 
+    if (!source.exists()) {
       System.out.println(String.format("The file: %s does not exist.", path));
       System.exit(1);
     }
-    
+
+    if (source.isFile() && !FilenameUtils.isExtension(source.getName(), SOURCE_EXTENSION)) {
+      System.out
+          .println(String.format("The file: %s must have a .vm extension.", source.getName()));
+      System.exit(1);
+    }
+
   }
 
-  
 }
--- projects/07/Nand2TetrisVMTranslator/vmtranslator/src/test/java/com/jasonrobertfox/nand2tetris/vmtranslator/CLITest.java
+++ projects/07/Nand2TetrisVMTranslator/vmtranslator/src/test/java/com/jasonrobertfox/nand2tetris/vmtranslator/CLITest.java
@@ -3,22 +3,40 @@ package com.jasonrobertfox.nand2tetris.vmtranslator;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.Assertion;
 import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import org.junit.rules.TemporaryFolder;
 
 public class CLITest
 {
   private static final String EOL = System.getProperty("line.separator");
   private static final String NON_EXISTANT_FILE = "nofile.txt";
+  private static final String BAD_EXTENSION = ".bad";
+  private static final String VALID_FILE_NAME = "vmsource";
 
+  private File badExtensionFile;
+  
   @Rule
   public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
   @Rule
   public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+
+  @Rule
+  public TemporaryFolder testFolder = new TemporaryFolder();
+  
+  
+  @Before
+  public void setUp() throws IOException {
+      badExtensionFile = File.createTempFile(VALID_FILE_NAME,BAD_EXTENSION);
+  }
 
   @Test
   public void itShouldShowUsageForNoArgument()
@@ -32,6 +50,13 @@ public class CLITest
   {
     expectAbortWithMessage(String.format("The file: %s does not exist.%n", NON_EXISTANT_FILE, EOL));
     CLI.main(NON_EXISTANT_FILE);
+  }
+
+  @Test
+  public void itShouldErrorForInvalidExtension() throws IOException
+  {
+    expectAbortWithMessage(String.format("The file: %s must have a .vm extension.%n",  badExtensionFile.getName(), EOL));
+    CLI.main(badExtensionFile.getCanonicalPath());
   }
 
   private void expectAbortWithMessage(final String errorMessage)
